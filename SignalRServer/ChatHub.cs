9edb569e7c639e9e959748f6dd572025e61ddab5
Camilo Jaramillo <camiloinfo07@gmail.com>
2024-02-15 10:39:15 -0500

    Start including signalR server (hubs)

---
+++ SignalRServer/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalRServer;
+
+using Microsoft.AspNetCore.SignalR;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string message)
+    {
+        // Validate message if needed
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+}
---
+++ SignalRServer/StockHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServer
+{
+    public class StockHub : Hub
+    {
+    }
+}
